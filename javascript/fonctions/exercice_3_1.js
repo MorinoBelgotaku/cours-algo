e8f92b8b646d6a7f1666c584c8770250b80dff07
Morino_Belgotaku <morinbruno@hotmail.be>
2023-12-12 18:20:32 +0100

    Upload

---
+++ javascript/fonctions/exercice_3_1.js
@@ -0,0 +1,27 @@
+const nombres = [10, 20, 144, 30, -56, 88, 33]
+
+function nombre_plus_grand_ou_petit(tableau, valeur) {
+    let petit_nombre = -Infinity;
+    let grand_nombre = Infinity;
+    if (valeur.toLocaleLowerCase() === 'grand') {
+        for (let i = 0; i < tableau.length; i++) {
+            if (tableau[i] > petit_nombre) {
+                petit_nombre = tableau[i]
+            }
+        }
+        return petit_nombre;
+    }
+    else if (valeur.toLocaleLowerCase() === 'petit') {
+        for (let i = 0; i < tableau.length; i++) {
+            if (tableau[i] < grand_nombre) {
+                grand_nombre = tableau[i]
+            }
+        }
+        return grand_nombre;
+    }
+    else {
+        return 'Donner un paramètre valide!'
+    }
+}
+
+console.log("Nombre plus grand du tableau : " + nombre_plus_grand_ou_petit(nombres, 'petit'))
