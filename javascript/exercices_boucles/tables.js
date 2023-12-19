8f3b80fb610d9ddc94b3318d845b9eba4421bdad
Morino_Belgotaku <morinbruno@hotmail.be>
2023-12-19 20:09:28 +0100

    New

---
+++ javascript/exercices_boucles/tables.js
@@ -0,0 +1,14 @@
+let i = 1;
+let nombre = 5;
+while (i <= 10) {
+    console.log(i + ' x ' + nombre + ' = ' + i * nombre)
+    i++
+}
+
+// -------------------------
+
+i = 1;
+while (i <= 10) {
+    console.log(i)
+    i++
+}
